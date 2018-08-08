da5a1ae1da091f2d9b2a2f3920f5e96029fc5075
cda4376 <42180285+cda4376@users.noreply.github.com>
2018-08-08 08:47:22 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Builds a list (object)
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(loc_inv) inv <<- loc_inv
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
+## Return inverse of x
+## x has to be a square matrix, it should not be computationally singular?
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  loc_x <- x$get()
+  inv <- solve(loc_x, ...)
+  x$setInverse(inv)
+  inv
 }
